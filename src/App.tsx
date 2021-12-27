27c06de8b50fc49d0117c7e459401dd27cdf837e
Fyodor <sivkov1998@yandex.ru>
2021-12-27 11:37:56 +0300

    added options component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,18 @@ import { CheckBoxList } from "./components/checkboxList/checkboxlist"
 import { DateInput } from "./components/datepicker/dateInput"
 import { Dropdown } from "./components/dropdown/dropdown"
 import { Input } from "./components/input/input"
+import { Options } from "./components/options/options"
 import { RichCkeckList } from "./components/richcheckboxlist/rickchecklist"
+import image1 from './assets/image1.svg'
+import image2 from './assets/image2.svg'
 
 const richCheckboxData = [
   {label: 'Широкий коридор', describtion: 'Ширина коридоров в номере не менее 91 см.'},
   {label: 'Помощник для инвалидов', describtion: 'На 1 этаже вас встретит специалист  и проводит до номера.'},
+]
+const optionsData = [
+  {image: image1, name: 'Комфорт', describtion: 'Шумопоглощающие стены'},
+  {image: image2, name: 'Удобство', describtion: 'Окно в каждой из спален'}
 ]
 
 export const App = () => {
@@ -21,6 +28,7 @@ export const App = () => {
       <CheckBoxList id="list" name="expandable checkbox list" list={['Завтрак', 'Письменный стол', 'Стул для кормления', 'Кроватка']} />
       <RichCkeckList name="rich Checkbox Buttons" data={richCheckboxData} />
       <BulletList name="Bullet list" data={['Нельзя с питомцами', 'Без вечеринок и мероприятий', 'Время прибытия — после 13:00, а выезд до 12:00']} />
+      <Options data={optionsData} />
     </div>
   )
 }
---
+++ src/components/options/options.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react"
+import css from './options.module.css'
+
+type data = {image: string, name: string, describtion: string}
+
+type props = {
+    data: data[]
+}
+
+export const Options: FC<props> = ({ data }) => {
+    return <section className={css.optionsContainer}>
+        {data.map( el => <Item data={el} />)}
+    </section>
+}
+
+type ItemProps = {
+    data: data
+}
+
+const Item:FC<ItemProps> = ({ data }) => {
+    return <div className={css.item}>
+        <img src={data.image} alt="pic" />
+        <div className={css.itemContent}>
+            <h4>{data.name}</h4>
+            <p>{data.describtion}</p>
+        </div>
+    </div>
+}
